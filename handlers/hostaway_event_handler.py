fedc06f0216a26a8a76e0cf43f10ee7eb04457ec
Austin Hendricks <austinghendricks@gmail.com>
2024-09-06 15:11:16 -0400

    Refactor event handler to improve reservation handling logic.

--- handlers/hostaway_event_handler.py
+++ handlers/hostaway_event_handler.py
@@ -6,8 +6,6 @@ import models
 
 def handle_event(object_type, event_type, data):
     """Handles Hostaway events"""
-    # Log the webhook event
-    notifier.inform(f"Data received from Hostaway webhook!")
 
     # Validate the webhook payload
     isValid, msg = validator.validate_hostaway_webhook_payload(data)
@@ -49,6 +47,9 @@ def __handle_reservation_event(event_type, reservation_obj):
         reservation_service.create_reservation(reservation_obj)
 
     elif event_type == "reservation.updated":
+        # Ensure referenced reservation exists in the database
+        __ensure_referenced_reservation_exists(reservation_obj["id"])
+
         # Handle reservation updates and create a revision entry
         reservation_service.update_reservation(reservation_obj)
 
@@ -56,11 +57,18 @@ def __handle_reservation_event(event_type, reservation_obj):
 def __handle_conversation_message_event(message_obj):
     """Handles conversationMessage events (message.received)"""
     # Ensure referenced reservation exists in the database
-    reservation_id = message_obj["reservationId"]
+    __ensure_referenced_reservation_exists(message_obj["reservationId"])
+
+    # Create a new conversationMessage entry
+    message_service.create_conversation_message(message_obj)
+
+
+def __ensure_referenced_reservation_exists(reservation_id):
+    """Ensures the referenced reservation exists in the database, polling Hostaway API if necessary"""
     reservation = db.session.get(models.Reservation, reservation_id)
     if not reservation:
         notifier.inform(
-            f"Conversation message received for missing reservation {reservation_id}. Polling Hostaway API for reservation data...",
+            f"Data received for missing reservation {reservation_id}. Polling Hostaway API for reservation data...",
         )
         # Reservation not found, try to fetch it from Hostaway API
         reservation_data = hostaway_client.get_reservation(reservation_id)
@@ -71,5 +79,3 @@ def __handle_conversation_message_event(message_obj):
             notifier.inform(
                 f"Failed to retrieve reservation {reservation_id} from Hostaway API."
             )
-
-    message_service.create_conversation_message(message_obj)
